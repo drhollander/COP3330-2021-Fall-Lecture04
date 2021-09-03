7d6348529305e3d7a0b8749fb7d1622ceda8de98
drhollander <chris.hollander@gmail.com>
2021-09-02 20:32:11 -0400

    Added pseudocode

--- src/main/java/lecture04/Example01.java
+++ src/main/java/lecture04/Example01.java
@@ -6,6 +6,9 @@ public class Example01 {
     private static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        // ask the user for a name
+        // read in the name from the command line
+        // print out the name to standard output
         System.out.print("What is your name? ");
         String name = in.nextLine();
         System.out.println("Hello, " + name + ", nice to meet you!");
